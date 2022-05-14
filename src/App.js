bedd9bfc13dac2099ef7fac4318758121f3f6ff8
INTERLOGIC\ysy <ysy@interlogic.com.ua>
2022-05-14 20:41:22 +0300

    check size

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 
 function App() {
   const heart = 'https://media3.giphy.com/media/xwx6UV2qgwLlDQ5MRW/giphy.gif';
+  const message = 'а я тебе люблю))))';
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={heart} className="App-logo" alt="logo" />
-        <p>
-          Я тебе люблю
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-        </p>
+        <h2>{message}</h2>
       </header>
     </div>
   );
